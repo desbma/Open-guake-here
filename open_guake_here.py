e853aaedb1ae0a49add8056f6bd26760fd1af4d8
desbma <desbma@users.noreply.github.com>
2014-08-24 12:00:01 +0200

    PEP257

--- open_guake_here.py
+++ open_guake_here.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 
 """
-    This module adds a menu item to the nautilus right-click menu which allows
-    to open the guake terminal with a new tab set on the current directory
+Add a Nautilus "Open Guake terminal" right click entry.
+
+This module adds a menu item to the nautilus right-click menu which allows to
+open the guake terminal with a new tab set on the current directory.
 """
 
 import locale
@@ -28,14 +30,11 @@ _ = gettext.gettext
 
 
 class OpenTerminalGeometry(GObject.GObject, Nautilus.MenuProvider):
-    """Implements the 'Open Guake here' extension to the nautilus right-click
-       menu
-    """
+
+    """ Implements the extension to the Nautilus right-click menu. """
 
     def __init__(self):
-        """Nautilus crashes if a plugin doesn't implement the __init__
-           method
-        """
+        """ Nautilus crashes if a plugin doesn't implement the __init__ method. """
         try:
             factory = Gtk.IconFactory()
             pixbuf = GdkPixbuf.Pixbuf.new_from_file(ICONPATH)
@@ -46,7 +45,7 @@ class OpenTerminalGeometry(GObject.GObject, Nautilus.MenuProvider):
             pass
 
     def run(self, menu, selected):
-        """Runs the Open Guake here on the given Directory"""
+        """ Open Guake in the given directory. """
         uri_raw = selected.get_uri()
         if len(uri_raw) < 7:
             return
@@ -57,9 +56,11 @@ class OpenTerminalGeometry(GObject.GObject, Nautilus.MenuProvider):
         subprocess.call(bash_string, shell=True)
 
     def get_file_items(self, window, sel_items):
-        """Adds the 'Open Guake here' menu item to the Nautilus right-click
-           menu, connects its 'activate' signal to the 'run' method passing the
-           selected Directory/File"""
+        """
+        Add the 'Open Guake here' menu item to the Nautilus right-click
+        menu, connects its 'activate' signal to the 'run' method passing the
+        selected directory/file.
+        """
         if len(sel_items) != 1 or sel_items[0].get_uri_scheme() != 'file':
             return
         item = Nautilus.MenuItem(name='NautilusPython::guake',
@@ -71,9 +72,11 @@ class OpenTerminalGeometry(GObject.GObject, Nautilus.MenuProvider):
         return [item]
 
     def get_background_items(self, window, current_directory):
-        """Adds the 'Open Guake here' menu item to the Nautilus right-click
-           menu, connects its 'activate' signal to the 'run' method passing the
-           current Directory"""
+        """
+        Add the 'Open Guake here' menu item to the Nautilus right-click
+        menu, connects its 'activate' signal to the 'run' method passing the
+        current directory.
+        """
         item = Nautilus.MenuItem(name='NautilusPython::guake',
                                  label=_('Open Guake Here'),
                                  tip=_("""Open the Guake terminal on the
